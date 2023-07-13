e4a64beaa88e9f1fa1b43bfc0249af454a55c40b
dilarakasak <dilarakasak7@gmail.com>
2023-07-13 17:34:09 +0300

    React Projesinin Oluşturulması

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA39g7M_ksaMDMb8ndTHcdot6-e3JGbHL4",
+    authDomain: "dilara-a7f97.firebaseapp.com",
+    projectId: "dilara-a7f97",
+    storageBucket: "dilara-a7f97.appspot.com",
+    messagingSenderId: "848561690161",
+    appId: "1:848561690161:web:449a6e85e0f98292dce710"
+  };
